3ae94bbae9d1d8d9d23fb701affb736165e48a29
Lukasz Pielak <pielak@gmail.com>
2014-03-30 20:29:17 +0200

    Published master to GitHub pages.

---
+++ githubdeploy.sh
@@ -0,0 +1,21 @@
+Using profile: production
+Generating site: http://babun.guthub.io
+Copying /Users/lukasz/Workspace/gitrepo/github/babun.github.io/README.md to generated site without processing; missing required gem -- redcarpet (or equivalent)
+undefined method `each' for nil:NilClass
+/Users/lukasz/Workspace/gitrepo/github/babun.github.io/_ext/tagger_patch.rb:12:in `execute'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/pipeline.rb:37:in `block in execute_extensions'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/pipeline.rb:36:in `each'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/pipeline.rb:36:in `execute_extensions'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/pipeline.rb:32:in `execute'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/engine.rb:235:in `execute_pipeline'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/engine.rb:66:in `run'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/cli/generate.rb:20:in `run'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/cli/invoker.rb:104:in `invoke_generate'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/lib/awestruct/cli/invoker.rb:47:in `invoke!'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/gems/awestruct-0.5.4.rc/bin/awestruct:9:in `<top (required)>'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/bin/awestruct:23:in `load'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/bin/awestruct:23:in `<main>'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/bin/ruby_executable_hooks:15:in `eval'
+/Users/lukasz/.rvm/gems/ruby-1.9.3-p545/bin/ruby_executable_hooks:15:in `<main>'
+Deploying to github_pages
+You have uncommitted changes in the working branch. Please commit or stash them.
