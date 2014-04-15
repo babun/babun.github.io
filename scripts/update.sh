28031bb5a6cd4bc6d0f2e89b3423b1ab926431b7
Lukasz Pielak <pielak@gmail.com>
2014-04-16 00:18:18 +0200

    simplified slim file, gitignore, new pictures

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,10 +4,7 @@ BABUN_DOC_REPO='https://raw.githubusercontent.com/babun/babun/master/babun-doc/a
 DOWNLOAD_DIR='../page_include'
 
 echo "Cleaning out old files"
-rm -f $DOWNLOAD_DIR"/development.adoc"
-rm -f $DOWNLOAD_DIR"/usage.adoc"
-rm -f $DOWNLOAD_DIR"/footer.adoc"
-rm -f $DOWNLOAD_DIR"/licence.adoc"
+rm -f $DOWNLOAD_DIR"/*.adoc"
 
 
 echo "Downloading document contents"
@@ -15,5 +12,6 @@ curl -o $DOWNLOAD_DIR"/development.adoc" $BABUN_DOC_REPO"/development.adoc"
 curl -o $DOWNLOAD_DIR"/usage.adoc" $BABUN_DOC_REPO"/usage.adoc"
 curl -o $DOWNLOAD_DIR"/footer.adoc" $BABUN_DOC_REPO"/footer.adoc"
 curl -o $DOWNLOAD_DIR"/licence.adoc" $BABUN_DOC_REPO"/licence.adoc"
+curl -o $DOWNLOAD_DIR"/supporters.adoc" $BABUN_DOC_REPO"/supporters.adoc"
 
 
