3f742e3812f6f036f6a0b7b5384253428a55466c
Tom Bujok <tom.bujok@gmail.com>
2014-04-27 23:01:35 +0200

    Cleanup

---
+++ scripts/all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./update.sh
+./deploy.sh
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,23 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 BABUN_DOC_REPO='https://raw.githubusercontent.com/babun/babun/master/babun-doc/adoc'
 DOWNLOAD_DIR='../page_include'
+DEV_DIR='../development'
 
 echo "Cleaning out old files"
-rm -f $DOWNLOAD_DIR"/*.adoc"
+rm -rf "$DOWNLOAD_DIR"
+rm -rf "$DEV_DIR"
+
+mkdir "$DOWNLOAD_DIR"
 
 echo "Downloading document contents"
-curl -o $DOWNLOAD_DIR"/development.adoc" $BABUN_DOC_REPO"/development.adoc"
-curl -o $DOWNLOAD_DIR"/usage.adoc" $BABUN_DOC_REPO"/usage.adoc"
-curl -o $DOWNLOAD_DIR"/footer.adoc" $BABUN_DOC_REPO"/footer.adoc"
-curl -o $DOWNLOAD_DIR"/licence.adoc" $BABUN_DOC_REPO"/licence.adoc"
-curl -o $DOWNLOAD_DIR"/supporters.adoc" $BABUN_DOC_REPO"/supporters.adoc"
+curl -o "$DOWNLOAD_DIR/../development.adoc" "$BABUN_DOC_REPO/development.adoc"
+curl -o "$DOWNLOAD_DIR/footer.adoc" "$BABUN_DOC_REPO/footer.adoc"
+curl -o "$DOWNLOAD_DIR/header.adoc" "$BABUN_DOC_REPO/header.adoc"
+curl -o "$DOWNLOAD_DIR/installation.adoc" "$BABUN_DOC_REPO/installation.adoc"
+curl -o "$DOWNLOAD_DIR/licence.adoc" "$BABUN_DOC_REPO/licence.adoc"
+curl -o "$DOWNLOAD_DIR/screencast.adoc" "$BABUN_DOC_REPO/screencast.adoc"
+curl -o "$DOWNLOAD_DIR/supporters.adoc" "$BABUN_DOC_REPO/supporters.adoc"
+curl -o "$DOWNLOAD_DIR/usage.adoc" "$BABUN_DOC_REPO/usage.adoc"
 
 
