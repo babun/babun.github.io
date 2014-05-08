f9d9a99eb32a70a4c54651283074d7cc78fff484
Lukasz Pielak <pielak@gmail.com>
2014-05-08 22:28:13 +0200

    regenerated page

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,12 @@
 BABUN_DOC_REPO='https://raw.githubusercontent.com/babun/babun/master/babun-doc/adoc'
 DOWNLOAD_DIR='../page_include'
 DEV_DIR='../development'
+SCREENSHOTS_DIR='../screenshots'
 
 echo "Cleaning out old files"
 rm -rf "$DOWNLOAD_DIR"
 rm -rf "$DEV_DIR"
+rm -rf "$SCREENSHOTS_DIR"
 
 mkdir "$DOWNLOAD_DIR"
 
